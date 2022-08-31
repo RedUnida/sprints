e14ba2a5497d5e9ffb7e53d292e6d3cf9032bd3f
Jerson Pacheco <yesid57@outlook.es>
2022-08-30 21:04:11 -0500

    commit jerson

---
+++ src/main/java/com/ProyectoCiclo4/Sprint1/Sprint1.java
@@ -0,0 +1,10 @@
+package com.ProyectoCiclo4.Sprint1;
+
+public class Sprint1 {
+    public static void main(String[] args) {
+
+        System.out.println("Hola, soy Jorge Andrés Goyeneche Galeano, Scrum Master del equipo Red Unida, saludos.");
+        System.out.println("Hola, soy Jerson Yesid Pacheco Castellanos, Desarrollador del equipo Red Unida, saludos.");
+
+    }
+}
